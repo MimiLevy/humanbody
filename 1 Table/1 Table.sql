c4b7f1f4c524cc957531bb8911c5bdaa37f64692
Mimi Levy <264mimi@gmail.com>
2023-02-15 09:47:14 +0200

    1st commit for the tria branch

--- 1 Table/1 Table.sql
+++ 1 Table/1 Table.sql
@@ -2,10 +2,13 @@ use HumanBodyDB
 go 
 drop table if exists body
 go 
-create table dbo.body(
+create table dbo.body
+    (
     id int not null identity primary key, 
     bodypart varchar(100) not null, 
-    num int not null)
+    num int not null
+	creaturetype varchar(25) not null
+	)
 go 
 
 
